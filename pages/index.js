5fe0fe90fa978354f06309b8935514c7569f0ab3
kano <soranotami0402@gmail.com>
2022-07-20 11:22:08 +0900

    a

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <h1>上映中映画</h1>
+    </Layout>
+  );
+}
